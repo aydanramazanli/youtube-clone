9a815b15689c145a0e934c269743eccda6191952
Aydan <aydan2903@gmail.com>
2022-08-01 14:37:01 +0400

    config firebase app

--- src/companents/header/Header.jsx
+++ src/companents/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./_header.scss";
 import { FaBars } from "react-icons/fa"
 import { AiOutlineSearch } from "react-icons/ai"
