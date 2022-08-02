4a0002acc3c9a1cb3fabed4e0e2b2210ba0e0d1b
Aydan <aydan2903@gmail.com>
2022-08-02 13:44:20 +0400

    must auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import Sidebar from "./companents/sidebar/Sidebar";
 import SideIcons from "./companents/sidebar/SideIcons";
 import Header from "./companents/header/Header";
@@ -7,17 +7,25 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { Container } from "react-bootstrap";
 import "./style/_base.scss";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-
-
+import "./App.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 const Layout = ({ children }) => {
-
+  const navigate=useNavigate()
   const [visible, setVisible] = useState(true);
 
   const handleClick = () => {
     setVisible(!visible);
   };
+
+  const {loading, accessToken} =useSelector(state=>state.auth)
+
+useEffect(()=>{
+if(!loading && !accessToken){
+  navigate("/auth")
+}
+},[loading,accessToken, navigate])
+
   return (
     <div className="app">
       <Header handleClick={handleClick} />
@@ -32,43 +40,40 @@ const Layout = ({ children }) => {
 };
 
 const App = () => {
-
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            exact
-            element={
-              <Layout>
-                <Home />
-              </Layout>
-            }
-          ></Route>
+      <Routes>
+        <Route
+          path="/"
+          exact
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        ></Route>
 
-          <Route path="/auth" element={<Login />} />
+        <Route path="/auth" element={<Login />} />
 
-          <Route
-            path="/search"
-            exact
-            element={
-              <Layout>
+        <Route
+          path="/search"
+          exact
+          element={
+            <Layout>
               <h1>Search Results</h1>
-              </Layout>
-            }
-          ></Route>
+            </Layout>
+          }
+        ></Route>
 
-<Route
-        path="*"
-         element={
-          <Layout>
-            <Home />
-          </Layout>
-        }
-    />
-        </Routes>
-      </BrowserRouter>
+        <Route
+          path="*"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+      </Routes>
     </>
   );
 };
